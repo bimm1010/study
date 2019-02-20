0404ffbed2de4c79de8304b620e8c8360cc40cd2
reishane <hoangnam69689>
2019-02-20 23:14:44 +0700

    student

---
+++ study.js
@@ -0,0 +1,9 @@
+var a, b, c;
+a = prompt ("Điểm vật lý");
+b = prompt ("Điểm hoá học");
+c = prompt ("Điểm sinh học");
+a = parseInt (a) ;
+b = parseInt (b) ;
+c = parseInt (c) ;
+document.write("Điểm trung bình 3 môn học là: " + ((a + b + c)  / 3) + "<br/>");
+document.write("Điểm tổng 3 môn học là: " + (a + b + c) + "<br/>");
