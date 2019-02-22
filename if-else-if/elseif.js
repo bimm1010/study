5be45f781f9152a8dcf68b12ea83cda2e8f73101
reishane <hoangnam69689>
2019-02-22 10:37:41 +0700

    iff else if

---
+++ if-else-if/elseif.js
@@ -0,0 +1,17 @@
+var userName = prompt("who's there?");
+if (userName == 'admin') {
+    alert('Welcome');
+} else if (userName == null) {
+    alert('Canceled');
+} else {
+    alert("I don't know you");
+}
+
+var pass = prompt('Password');
+if (Password == TheMaster) {
+    alert('Welcome');
+} else if (Password == null) {
+    alert('Cancecel');
+} else {
+    alert("Wrong password");
+}
